c3c3e0883ce69d8e03524458599d39bce911d575
chavitag <xavi@iesrodeira.com>
2018-10-11 00:30:48 +0200

    Engadidos comentarios a ParOuImpar.ps1

--- Tema 02/PowerShell/ParOuImpar.ps1
+++ Tema 02/PowerShell/ParOuImpar.ps1
@@ -1,3 +1,12 @@
+# Indica si un número é par ou impar
+#
+#	Pedir numero
+#	Si (o resto de dividir o número entre dous é igual a 0) entón 
+#		visualizar "O Número " numero "é Par"
+#	Se non 
+#		visualizar "O Número " numero "é IMPAR"
+#	Fin-Si
+
 [int]$num=Read-Host "Introduce un número "
 if ($num % 2 -eq 0) {
 	Write-Host "O Número $num é PAR"
