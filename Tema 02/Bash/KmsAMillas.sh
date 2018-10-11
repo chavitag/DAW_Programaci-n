c9f6905187eed1c27d0fe32236ecfd1718b8f2a6
chavitag <xavi@iesrodeira.com>
2018-10-11 13:15:07 +0200

    Incorporado KmsAMillas

---
+++ Tema 02/Bash/KmsAMillas.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Escribir un algoritmo que pida un número de Kms e nos visualice o seu valor en millas
+#
+# PSEUDOCÓDIGO
+#
+#  Pedir Kms
+#  Millas = Kms/1.60934
+
+echo "Introduce un número: "
+read kilometros
+# Bash non soporta operacións con decimais, debemos usar o comando bc, que é unha "calculadora". 
+# a variable scale indica os decimais a utilizar
+millas=$(echo "scale=6; $kilometros/1.60934;" | bc)
+echo "$kilometros Kms son $millas Millas"
